816cc85c1e018da35b1f1ce754c4636b63b5124c
Abhijeet Satpute <abhi111sat@gmail.com>
2022-04-24 16:17:58 +0530

    Dynamic list rendering using year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,15 +1,33 @@
+import { useState } from 'react';
 import './Expenses.css';
 import ExpenseItem from "./ExpenseItem";
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
 
 const Expenses = (props) => {
+    const [filteredYear, setFilteredYear] = useState('2020');
+    const selectHandler = selectedYear => {
+        setFilteredYear(selectedYear);
+    }
+    
+    let newFilteredExpenses = props.expenses.filter(expense => 
+        expense.date.getFullYear().toString() === filteredYear
+    )
+
     return (
-        <Card className='expenses'>
-            <ExpenseItem title={props.expenses[0].title} amount={props.expenses[0].amount} date={props.expenses[0].date} />
-            <ExpenseItem title={props.expenses[1].title} amount={props.expenses[1].amount} date={props.expenses[1].date} />
-            <ExpenseItem title={props.expenses[2].title} amount={props.expenses[2].amount} date={props.expenses[2].date} />
-            <ExpenseItem title={props.expenses[3].title} amount={props.expenses[3].amount} date={props.expenses[3].date} />
-        </Card>
+        <div>
+            <Card className='expenses'>
+                <ExpensesFilter selected={filteredYear} onYearSelect={selectHandler} />
+                {newFilteredExpenses.map(expense => (
+                    <ExpenseItem
+                        key={expense.id}
+                        title={expense.title}
+                        amount={expense.amount}
+                        date={expense.date}
+                    />
+                ))}
+            </Card>
+        </div>
     )
 }
 
